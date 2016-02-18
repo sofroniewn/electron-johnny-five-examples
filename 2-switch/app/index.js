d7daee29940dfeba4a95294a2b77b9dafea98649
Nicholas Sofroniew <sofroniewn@janelia.hhmi.org>
2016-02-18 17:53:49 -0500

    fix formatting

--- 2-switch/app/index.js
+++ 2-switch/app/index.js
@@ -1,25 +1,25 @@
-var Readable = require("stream").Readable;  
-var util = require("util");  
-util.inherits(MyStream, Readable);  
+var Readable = require('stream').Readable  
+var util = require('util')  
+var five = require('johnny-five')
+
+util.inherits(MyStream, Readable)  
 function MyStream(opt) {  
-  Readable.call(this, opt);
+  Readable.call(this, opt)
 }
 MyStream.prototype._read = function() {};  
 // hook in our stream
-process.__defineGetter__("stdin", function() {  
-  if (process.__stdin) return process.__stdin;
-  process.__stdin = new MyStream();
-  return process.__stdin;
-});
+process.__defineGetter__('stdin', function() {  
+  if (process.__stdin) return process.__stdin
+  process.__stdin = new MyStream()
+  return process.__stdin
+})
 
-
-var five = require('johnny-five')
 var board = new five.Board()
 
 board.on('ready', function() {
-  document.getElementById('board-status').src = "icons/ready.png"
-  var led = new five.Led(12);
-  var spdt = new five.Switch(9)	
+  document.getElementById('board-status').src = 'icons/ready.png'
+  var led = new five.Led(12)
+  var spdt = new five.Switch(9)
 
   spdt.on('open', function() {
     led.off()
@@ -30,5 +30,4 @@ board.on('ready', function() {
     led.on()
     document.getElementById('led-status').src = 'icons/led-on.png'
   })
-
-});
+})
