d861aa95ed02e4aedb071a219e18699babfb4370
Nicholas Sofroniew <sofroniewn@janelia.hhmi.org>
2016-02-18 17:15:21 -0500

    Correct indentation

--- 2-switch/main.js
+++ 2-switch/main.js
@@ -4,15 +4,15 @@ var BrowserWindow = require('browser-window')
 var mainWindow = null
 
 app.on('ready', function () {
-	mainWindow = new BrowserWindow({
-		height: 475,
-		resizable: false,
-		width: 595
-	})
+  mainWindow = new BrowserWindow({
+    height: 475,
+    resizable: false,
+    width: 595
+  })
 
-	mainWindow.loadURL('file://' + __dirname + '/app/index.html')
+  mainWindow.loadURL('file://' + __dirname + '/app/index.html')
 
-	mainWindow.on('closed', function () {
-		mainWindow = null
-	})
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
 })
--- 8-sensor-strobe/main.js
+++ 8-sensor-strobe/main.js
@@ -4,15 +4,15 @@ var BrowserWindow = require('browser-window')
 var mainWindow = null
 
 app.on('ready', function () {
-	console.log('main')
-	mainWindow = new BrowserWindow({
-		height: 600,
-		width: 800
-	})
+  mainWindow = new BrowserWindow({
+    height: 600,
+    resizable: false,
+    width: 800
+  })
 
-	mainWindow.loadURL('file://' + __dirname + '/app/index.html')
+  mainWindow.loadURL('file://' + __dirname + '/app/index.html')
 
-	mainWindow.on('closed', function () {
-		mainWindow = null
-	})
+  mainWindow.on('closed', function () {
+    mainWindow = null
+  })
 })
